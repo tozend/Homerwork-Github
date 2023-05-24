5cd3356f36615f9bada687bf54edb8c3ed2f7b98
Tomasz Zenderowski <tomasz.zenderowski@globallogic.com>
2023-05-24 18:17:14 +0200

    Added Dockerfile, refactored UI tests

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
 import pytest
+from selenium import webdriver
 
 from src.applications.api.github_api_client import GitHubAPIClient
 from src.config.config import Config
+from src.applications.ui.github_app import GitHubApp
 
 
 @pytest.fixture(scope='module')
@@ -17,3 +19,30 @@ def fixture_github_api_client():
     yield api
 
     api.logout()
+
+
+@pytest.fixture
+def web_driver():
+    """
+    Simple version of fixture for tests.
+    Opens Chrome browser, returns webdriver object, quits browser.
+    """
+    driver = webdriver.Chrome()
+
+    yield driver
+
+    driver.quit()
+
+
+@pytest.fixture
+def github_ui():
+    """
+    Fixture for GitHub UI tests.
+    Opens selected browser, returns GitHubApp object, quits browser.
+    """
+    github_app = GitHubApp(browser_name="chrome")
+    github_app.launch_the_browser()
+
+    yield github_app
+
+    github_app.close_browser()
